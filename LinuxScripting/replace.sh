aa09e7d9eb90a45b77f6d6fea44af0f34a5ff1a0
acepoi <acepoi@Ubuntu22.03.4.myguest.virtualbox.org>
2023-12-11 11:49:40 +0200

    update replace.sh

--- LinuxScripting/replace.sh
+++ LinuxScripting/replace.sh
@@ -6,16 +6,19 @@ replacement_word="AUTOMATION"
 
 > $output_log
 
-for line_number in {63..69}
-do
-	word_to_replace=$(awk -v line="$line_number" '{if (NR==line) print $6}' "$log_file")
-	
-	sed -i "${line_number}s/\b${word_to_replace}\b/$replacement_word/" "$log_file"
-	
-	replaced_letters=$(echo "$replacement_word" | sed 's/./& /g')
-	
-	echo "<$word_to_replace> was replaced with the letter: [$replaced_letters]" >> "$output_log"
+replaced_letters=($(echo "$replacement_word" | sed 's/./& /g'))
 
+for line_number in {63..72}
+do
+        
+        word_to_replace=$(awk -v line="$line_number" '{if (NR==line) print $6}' "$log_file")
+        
+	sed -i "${line_number}s/\b${word_to_replace}\b/${replaced_letters[0]}/" "$log_file"
+	
+	echo "<$word_to_replace> was replaced with the letter: $replaced_letters" >> "$output_log"
+	
+	replaced_letters=("${replaced_letters[@]:1}")
+   	
 done
 
 echo "Replacement complete"
